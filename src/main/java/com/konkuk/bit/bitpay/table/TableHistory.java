c7c2199d0992287d0484c246022ea1add0fd0a0c
jaemin <anjm1020@gmail.com>
2023-05-12 20:55:33 +0900

    Feat : 테이블 히스토리 엔티티 작성

---
+++ src/main/java/com/konkuk/bit/bitpay/table/TableHistory.java
@@ -0,0 +1,28 @@
+package com.konkuk.bit.bitpay.table;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TableHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "table_history_id")
+    private Long id;
+
+    @Column(name = "table_history_timestamp")
+    private Date timestamp;
+
+    @Column(name = "table_history_type")
+    private String type;
+
+    @Column(name = "table_history_description")
+    private String description;
+}
