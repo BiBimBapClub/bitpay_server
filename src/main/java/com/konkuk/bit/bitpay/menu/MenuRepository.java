59394f9a4cfd2934238841a715a6d2e757968331
sejoon00 <parksj313@konkuk.ac.kr>
2023-05-14 23:35:51 +0900

    Feat : 도메인및 service interface 작성

--- src/main/java/com/konkuk/bit/bitpay/menu/MenuRepository.java
+++
@@ -1,2 +0,0 @@
-package com.konkuk.bit.bitpay.menu;public interface MenuRepository {
-}
---
+++ src/main/java/com/konkuk/bit/bitpay/menu/web/Dto/MenuUpdateDto.java
@@ -0,0 +1,14 @@
+package com.konkuk.bit.bitpay.menu.web.Dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class MenuUpdateDto {
+    private Integer remain;
+}
