1325925475f61570283dfc876d8c57567548780d
kangCoder <khsofficial1213@gmail.com>
2023-05-14 23:38:24 +0900

    Feat: 1차 push

--- src/main/java/com/konkuk/bit/bitpay/tablehistory/repository/TableHistoryRepository.java
+++ src/main/java/com/konkuk/bit/bitpay/tablehistory/repository/TableHistoryRepository.java
@@ -2,7 +2,6 @@ package com.konkuk.bit.bitpay.tablehistory.repository;
 
 import com.konkuk.bit.bitpay.tablehistory.domain.TableHistory;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TableHistoryRepository extends JpaRepository<TableHistory, Long> {
