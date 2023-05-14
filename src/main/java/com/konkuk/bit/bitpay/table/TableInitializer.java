8fcabb058fbca224135e843c45a40a8de4e38d64
ehdtndla123 <ehdtndla123@naver.com>
2023-05-15 08:54:19 +0900

    Fix : 테이블 History 관리

--- src/main/java/com/konkuk/bit/bitpay/table/TableInitializer.java
+++ src/main/java/com/konkuk/bit/bitpay/table/TableInitializer.java
@@ -1,21 +1,27 @@
 package com.konkuk.bit.bitpay.table;
 
+import com.konkuk.bit.bitpay.order.dto.OrderDto;
 import com.konkuk.bit.bitpay.table.domain.Table;
 import com.konkuk.bit.bitpay.table.domain.TableStatus;
+import com.konkuk.bit.bitpay.table.dto.TableDto;
 import com.konkuk.bit.bitpay.table.repository.TableRedisRepository;
+import com.konkuk.bit.bitpay.tablehistory.service.TableHistoryServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 @RequiredArgsConstructor
 public class TableInitializer implements CommandLineRunner {
 
     private final TableRedisRepository tableRepository;
+    private final TableHistoryServiceImpl tableHistoryService;
 
     private static final int MAX_TABLE_NUMBER = 45;
 
@@ -32,11 +38,23 @@ public class TableInitializer implements CommandLineRunner {
                     .status(TableStatus.CLEAN.getStatus())
                     .updatedTime(LocalDateTime.now())
                     .build();
-            tableRepository.save(table);
+            Table save = tableRepository.save(table);
+            TableDto tableDto = convertToTableDto(save);
+            tableHistoryService.createTableHistory(tableDto, "ALL");
         }
     }
 
     private String generateRedisKey(Integer tableNumber) {
         return "table:" + tableNumber;
+    }
+
+    private TableDto convertToTableDto(Table table) {
+        TableDto tableDto = new TableDto();
+        tableDto.setNumber(table.getNumber());
+        tableDto.setUuid(table.getUuid());
+        tableDto.setStatus(table.getStatus());
+        tableDto.setUpdatedTime(table.getUpdatedTime());
+        Integer value = Integer.parseInt(tableDto.getNumber().substring("table:".length()));
+        return tableDto;
     }
 }
