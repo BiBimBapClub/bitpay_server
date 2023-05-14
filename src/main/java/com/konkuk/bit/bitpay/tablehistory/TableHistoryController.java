5e553e78fd4acee3fcce1ad2b7989ee5f52dbdc1
kangCoder <khsofficial1213@gmail.com>
2023-05-15 02:31:17 +0900

    Feat: createOrderHistory 작성

--- src/main/java/com/konkuk/bit/bitpay/tablehistory/TableHistoryController.java
+++ src/main/java/com/konkuk/bit/bitpay/tablehistory/TableHistoryController.java
@@ -1,5 +1,10 @@
 package com.konkuk.bit.bitpay.tablehistory;
 
+import com.konkuk.bit.bitpay.tablehistory.domain.TableHistory;
+import com.konkuk.bit.bitpay.tablehistory.dto.TableHistoryDto;
+import com.konkuk.bit.bitpay.tablehistory.service.TableHistoryService;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,12 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 /** Admin Page 에서 지금까지 주문 로그를 요청하기 위한 Controller */
 @Slf4j
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/admin")
 public class TableHistoryController {
 
+    private final TableHistoryService tableHistoryService;
+
     //해당 테이블에 대한 모든 주문 기록 로그 가져오기 (시간 내림차순)
-    @GetMapping("/history")
-    public ResponseEntity<Void> getTableHistory(@RequestParam Integer tableNumber) {
-        return ResponseEntity.ok().build();
+    @GetMapping("/log")
+    public List<TableHistoryDto> getTableHistory(@RequestParam Integer tableNumber) {
+        return tableHistoryService.getHistories(tableNumber);
     }
 }
