31848d032232f66617cbdad5151d42eeb8ac13e7
kangCoder <khsofficial1213@gmail.com>
2023-05-15 19:39:46 +0900

    Feat: cors delete 추가

--- src/main/java/com/konkuk/bit/bitpay/tablehistory/TableHistoryController.java
+++ src/main/java/com/konkuk/bit/bitpay/tablehistory/TableHistoryController.java
@@ -1,13 +1,10 @@
 package com.konkuk.bit.bitpay.tablehistory;
 
-import com.konkuk.bit.bitpay.tablehistory.domain.TableHistory;
 import com.konkuk.bit.bitpay.tablehistory.dto.TableHistoryDto;
 import com.konkuk.bit.bitpay.tablehistory.service.TableHistoryService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
